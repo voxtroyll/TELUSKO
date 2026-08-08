5a64f15811e7e93da153a24b6449617afbbeea16
Voxtroyll <Voxtroyll@gmail.com>
2026-08-08 14:34:54 +0530

    Added Boxing.java and AutoBoxing.java

--- WrapperClass/src/Demo.java
+++ WrapperClass/src/Demo.java
@@ -1,6 +1,9 @@
 public class Demo {
     static void main(String[] args) {
         int num = 7;
-        Integer num1 = new Integer(8);
+        Integer num1= new Integer(8);
+        //Integer num1 = new Integer(8);
+
+        System.out.println(num1);
     }
 }
